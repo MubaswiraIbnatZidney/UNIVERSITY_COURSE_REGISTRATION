c060aefd388311b426c4d5940c0158a6c8d2fa2b
MubaswiraIbnatZidney <168271180+MubaswiraIbnatZidney@users.noreply.github.com>
2024-07-05 20:42:25 +0600

    Admission

---
+++ admission.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hogwarts University</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="top">
+            <div class="logo_area">
+                <a href="home.php">
+                    <img src="images/hogwartslogo.png" alt="logo">
+                    <h1> Hogwarts University </h1>
+                </a>
+            </div>
+            <div class="menu_area">
+                <ul>
+                    <li><a href="home.php">HOME</a></li>
+                    <li><a href="about.php">ABOUT US</a></li>
+                    <li><a href="department.php">DEPARTMENT</a></li>
+                    <li><a href="admission.php">ADMISSION</a></li>
+                    <li><a href="contact.php">CONTACT</a></li>
+                </ul>
+            </div>
+     </div>
+
+     <div class="admission-contact"> 
+            <h1>Want to get admitted here?</h1>
+            <br>
+            <p> Admission to Hogwarts typically occurs at the age of twenty, and <br> acceptance letters are delivered by owl post.<br> 
+                Letter is often delivered in a more creative and persistent manner to ensure the magical world is revealed to the student </p>
+
+                <br>
+                <div class="btn">
+                    <button><a href="http://admission.aust.edu/ug-admission/"> Get Admission </a></button>
+                </div>
+                <br>
+        </div>
+
+
+    </div> 
+</body>
+</html>

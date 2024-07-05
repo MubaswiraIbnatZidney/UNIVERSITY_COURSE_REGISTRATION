ba1144e4174ad655fedbe21fb390cf0dc11b6c76
MubaswiraIbnatZidney <168271180+MubaswiraIbnatZidney@users.noreply.github.com>
2024-07-05 20:42:59 +0600

    Contact

---
+++ contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hogwarts University</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="top">
+            <div class="logo_area">
+                <a href="home.php">
+                    <img src="images/hogwartslogo.png" alt="logo">
+                    <h1> Hogwarts University </h1>
+                </a>
+            </div>
+            <div class="menu_area">
+                <ul>
+                    <li><a href="home.php">HOME</a></li>
+                    <li><a href="about.php">ABOUT US</a></li>
+                    <li><a href="department.php">DEPARTMENT</a></li>
+                    <li><a href="admission.php">ADMISSION</a></li>
+                    <li><a href="contact.php">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+                <img class="hogletter" src="images/hogwartsletter.png" alt="...">
+    </div> 
+    
+</body>
+</html>
